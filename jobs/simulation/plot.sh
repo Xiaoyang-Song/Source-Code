bccd231262caf3656daec4d6362c87728931743b
xysong <xysong25@gmail.com>
2026-04-29 22:17:46 -0400

    Sync

---
+++ jobs/simulation/plot.sh
@@ -0,0 +1,3 @@
+
+
+python jobs/simulation/plot_sensitivity_new.py --n-values 25 --fixed-p 6
--- jobs/simulation/plot_sensitivity.sh
+++ jobs/simulation/plot_sensitivity.sh
@@ -1,11 +1,6 @@
-python jobs/simulation/plot_sensitivity_new.py --p-values 2,4,6,8,10 --n-values 15,20,25,30,35 --plot-best-baseline
-
-python jobs/simulation/plot_sensitivity_new.py --p-values 4,6,8 --n-values 20,25,30,35 --plot-best-baseline
-
-
 # Fixed p
 python jobs/simulation/plot_sensitivity_new.py \
-    --n-values 15,20,25,30,35 \
+    --n-values 25 \
     --plot-combinations \
     --combo-method DCCA \
     --fixed-p 6
